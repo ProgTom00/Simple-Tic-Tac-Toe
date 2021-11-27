116b163377812e04f281a2fdb5b616a0d287e8ed
Tomasz Olechowski <tonaszolechowski1992@gmail.com>
2021-11-27 23:30:59 +0100

    Add main logic for game

---
+++ src/SimpleTicTacToe/Main.java
@@ -0,0 +1,9 @@
+package SimpleTicTacToe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SimpleTicTacToe simpleTicTacToe = new SimpleTicTacToe();
+        simpleTicTacToe.playGame();
+    }
+}
